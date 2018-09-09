869af5bdc82f3ed4baddba7506faadb348f84bca
DasElius <elias.moesch@gmx.at>
2018-09-09 17:17:11 +0200

    Initial commit

---
+++ src/main/java/com/daselius/knockout/KnockOut.java
@@ -0,0 +1,27 @@
+package com.daselius.knockout;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * JavaDoc this file!
+ * Created: 09.09.2018
+ *
+ * @author DasElius
+ */
+public class KnockOut extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+}
